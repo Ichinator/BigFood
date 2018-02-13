818050041b2f11af64ecacee5278918a658371ab
Ichinator <arnolddouwma@orange.fr>
2018-02-13 14:41:49 +0100

    VichUploaderBundle is configured, we can upload images of food items and render this in a view

--- src/Ichinator/CommandBundle/Entity/Burger.php
+++ src/Ichinator/CommandBundle/Entity/Burger.php
@@ -3,10 +3,13 @@
 namespace Ichinator\CommandBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Burger
- *
+ * @ORM\Entity
+ * @Vich\Uploadable
  * @ORM\Table(name="burger")
  * @ORM\Entity(repositoryClass="Ichinator\CommandBundle\Repository\BurgerRepository")
  */
@@ -42,6 +45,23 @@ class Burger
      */
     private $description;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="burgers_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     /**
      * Get id
@@ -123,5 +143,57 @@ class Burger
     public function getDescription()
     {
         return $this->description;
+    }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Burger
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
     }
 }
