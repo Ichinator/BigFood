9cfaeb6317627e785a85505ccef1ecb371080360
Ichinator <arnolddouwma@orange.fr>
2018-02-13 17:07:48 +0100

    Entity name are displayed in the form

--- src/Ichinator/CommandBundle/Controller/DefaultController.php
+++ src/Ichinator/CommandBundle/Controller/DefaultController.php
@@ -5,15 +5,20 @@ namespace Ichinator\CommandBundle\Controller;
 use Ichinator\CommandBundle\Entity\Command;
 use Ichinator\CommandBundle\Form\CommandType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $command = new Command();
         $form = $this->createForm(CommandType::class, $command);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            return $this->redirectToRoute('homepage');
+        }
         $formView = $form->createView();
-        
+
         return $this->render('IchinatorCommandBundle:Default:command.html.twig', array('form' => $formView));
     }
 }
--- src/Ichinator/CommandBundle/Form/CommandType.php
+++ src/Ichinator/CommandBundle/Form/CommandType.php
@@ -2,11 +2,16 @@
 
 namespace Ichinator\CommandBundle\Form;
 
+use Ichinator\CommandBundle\Entity\Burger;
+use Ichinator\CommandBundle\Entity\Dessert;
+use Ichinator\CommandBundle\Entity\Plat;
+use Ichinator\CommandBundle\IchinatorCommandBundle;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class CommandType extends AbstractType
 {
@@ -15,18 +20,22 @@ class CommandType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+       // $emBurger = $this->getDoctrine
+
         $builder
-            ->add('burger', ChoiceType::class, array(
-                'placeholder' => 'Choisissez un burger',
-                'required' => false,
+            ->add('burger', EntityType::class, array(
+                'class' => Burger::class,
+                'choice_label' => 'name',
             ))
-            ->add('plat', ChoiceType::class, array(
-                'placeholder' => 'Choisissez un plat',
-                'required' => false,
+            ->add('plat', EntityType::class, array(
+                'class' => Plat::class,
+                'choice_label' => 'name',
             ))
-            ->add('dessert', ChoiceType::class, array(
-                'placeholder' => 'Choisissez un plat',
-                'required' => false,
+            ->add('dessert', EntityType::class, array(
+                'class' => Dessert::class,
+                'choice_label' => 'name',
+                /*'placeholder' => 'Choisissez un plat',
+                'required' => false,*/
             ))
             ->add('submit', SubmitType::class)
         ;
