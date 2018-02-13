d6adf771f74a9ca4aa5d10be5e5257ff5f1d3929
Ichinator <arnolddouwma@orange.fr>
2018-02-13 17:27:11 +0100

    When you send the form you are redirect to the homepage and there is a flash message. No flush in database yet

--- src/Ichinator/CommandBundle/Controller/DefaultController.php
+++ src/Ichinator/CommandBundle/Controller/DefaultController.php
@@ -15,6 +15,11 @@ class DefaultController extends Controller
         $form = $this->createForm(CommandType::class, $command);
         $form->handleRequest($request);
         if($form->isSubmitted()){
+            $burger = $form["burger"]->getData();
+            $plat = $form["plat"]->getData();
+            $dessert = $form["dessert"]->getData();
+
+            $this->addFlash('success', 'Votre commande a bien été enregistrée !');
             return $this->redirectToRoute('homepage');
         }
         $formView = $form->createView();
